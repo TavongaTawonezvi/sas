730aed57259357523a3e1a4554ef1f6853d338e2
Tavonga Tawonezvi <ttawonezvi13@gmail.com>
2021-07-17 08:14:04 +0200

    added wit bg / reveal

--- index.js
+++ index.js
@@ -2,18 +2,23 @@
 //reveal divs
 function showcol1(){
     document.getElementById("col1").style.visibility="visible";
-}setTimeout("showcol1()", 1500);
+}
 function showcol2(){
     document.getElementById("col2").style.visibility="visible";
-}setTimeout("showcol2()", 2500);
+}
 function showcol3(){
     document.getElementById("col3").style.visibility="visible";
-}setTimeout("showcol3()", 4000);
+}
+setTimeout("showcol1()", 2000);
+setTimeout("showcol2()", 3000);
+setTimeout("showcol3()", 4800);
 
 // hide all divs
 function hideDivs(){
     document.getElementById("col1").style.visibility="hidden";
     document.getElementById("col2").style.visibility="hidden";
     document.getElementById("col3").style.visibility="hidden";
-}setTimeout("hideDivs()", 5400);
+}setTimeout("hideDivs()", 5900);
     
+ 
+setTimeout(() => document.querySelector("body").style.background = `url(wit1.jpg)`, 6000);
